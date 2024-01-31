b5d175c58bbefdbf9633d0bfc9f83a4c4361948d
GliczDev <67753196+GliczDev@users.noreply.github.com>
2024-01-31 22:56:14 +0100

    Match latest SkAnalyzer dev changes

--- main.go
+++ main.go
@@ -15,8 +15,8 @@ var jvm *jnigi.JVM
 var env *jnigi.Env
 var skAnalyzer *jnigi.ObjectRef
 
-//export Init
-func Init(javaHome *C.char, analyzerJar *C.char) {
+//export InitJava
+func InitJava(javaHome *C.char, analyzerJar *C.char) {
 	if err := jnigi.LoadJVMLib(C.GoString(javaHome) + "/bin/server/jvm.dll"); err != nil {
 		log.Fatal(err)
 	}
@@ -30,48 +30,112 @@ func Init(javaHome *C.char, analyzerJar *C.char) {
 
 	runtime.LockOSThread()
 	jvm.AttachCurrentThread()
-
-	initAnalyzer()
 }
 
-func findAnalyzerFlag(flagName string) *jnigi.ObjectRef {
-	flag := jnigi.NewObjectRef("me/glicz/skanalyzer/AnalyzerFlag")
-	if err := env.GetStaticField("me/glicz/skanalyzer/AnalyzerFlag", flagName, flag); err != nil {
+func getEnumField(class string, index int) *jnigi.ObjectRef {
+	values := jnigi.NewObjectArrayRef(class)
+	if err := env.CallStaticMethod(class, "values", values); err != nil {
 		log.Fatal(err)
 	}
-	return flag
+	return env.FromObjectArray(values)[index]
 }
 
-func initAnalyzer() {
-	flags := []*jnigi.ObjectRef{
-		findAnalyzerFlag("FORCE_VAULT_HOOK"),
-		findAnalyzerFlag("FORCE_REGIONS_HOOK"),
-		findAnalyzerFlag("ENABLE_PLAIN_LOGGER"),
+type AnalyzerFlag uint8
+
+const (
+	ForceVaultHook AnalyzerFlag = 1 << iota
+	ForceRegionsHook
+)
+
+func findAnalyzerFlags(analyzerFlags AnalyzerFlag) []*jnigi.ObjectRef {
+	var flags []*jnigi.ObjectRef
+
+	if analyzerFlags&ForceVaultHook != 0 {
+		flags = append(flags, findAnalyzerFlag(0))
 	}
-	flagsObjectArray := env.ToObjectArray(flags, "me/glicz/skanalyzer/AnalyzerFlag")
+
+	if analyzerFlags&ForceRegionsHook != 0 {
+		flags = append(flags, findAnalyzerFlag(1))
+	}
+
+	return flags
+}
+
+func findAnalyzerFlag(analyzerFlag AnalyzerFlag) *jnigi.ObjectRef {
+	return getEnumField("me/glicz/skanalyzer/AnalyzerFlag", int(analyzerFlag))
+}
+
+type LoggerType uint8
+
+const (
+	Disabled LoggerType = iota
+	Normal
+	Plain
+)
+
+func findLoggerType(loggerType LoggerType) *jnigi.ObjectRef {
+	return getEnumField("me/glicz/skanalyzer/LoggerType", int(loggerType))
+}
+
+func javaFile(path *C.char) *jnigi.ObjectRef {
+	if path == nil {
+		return jnigi.NewObjectRef("java/io/File")
+	}
+
+	file, err := env.NewObject("java/io/File", c2javaString(path))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return file
+}
+
+//export InitAnalyzer
+func InitAnalyzer(analyzerFlags AnalyzerFlag, loggerType LoggerType, workingDir *C.char) {
+	flags := env.ToObjectArray(findAnalyzerFlags(analyzerFlags), "me/glicz/skanalyzer/AnalyzerFlag")
 
 	var err error
-	skAnalyzer, err = env.NewObject("me/glicz/skanalyzer/SkAnalyzer", flagsObjectArray, jnigi.NewObjectRef("java/io/File"))
+	skAnalyzer, err = env.NewObject("me/glicz/skanalyzer/SkAnalyzer", flags, findLoggerType(loggerType), javaFile(workingDir))
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func javaString(str string) *jnigi.ObjectRef {
-	javaStr, err := env.NewObject("java/lang/String", []byte(str))
+func c2javaString(str *C.char) *jnigi.ObjectRef {
+	javaStr, err := env.NewObject("java/lang/String", []byte(C.GoString(str)))
 	if err != nil {
 		log.Fatal(err)
 	}
 	return javaStr
 }
 
-//export Parse
-func Parse(path *C.char) {
-	future := jnigi.NewObjectRef("java/util/concurrent/CompletableFuture")
-	if err := skAnalyzer.CallMethod(env, "parseScript", future, javaString(C.GoString(path))); err != nil {
+func java2cString(str *jnigi.ObjectRef) *C.char {
+	var bytes []byte
+	if err := str.CallMethod(env, "getBytes", &bytes); err != nil {
 		log.Fatal(err)
 	}
-	env.DeleteLocalRef(future)
+	return C.CString(string(bytes))
+}
+
+//export Parse
+func Parse(path *C.char) *C.char {
+	future := jnigi.NewObjectRef("java/util/concurrent/CompletableFuture")
+	if err := skAnalyzer.CallMethod(env, "parseScript", future, c2javaString(path)); err != nil {
+		log.Fatal(err)
+	}
+
+	joinResult := jnigi.NewObjectRef("java/lang/Object")
+	if err := future.CallMethod(env, "join", joinResult); err != nil {
+		log.Fatal(err)
+	}
+
+	scriptStructure := joinResult.Cast("me/glicz/skanalyzer/ScriptAnalyzeResult")
+
+	jsonResult := jnigi.NewObjectRef("java/lang/String")
+	if err := scriptStructure.GetField(env, "jsonResult", jsonResult); err != nil {
+		log.Fatal(err)
+	}
+
+	return java2cString(jsonResult)
 }
 
 //export Exit
